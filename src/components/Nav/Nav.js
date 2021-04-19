587d941391821f56ed3b4a844d4b732c61340767
wdfhai <wd.fhai@outlook.com>
2021-04-19 11:31:03 -0500

    removed jquery using useState and classnames; fixed credit text positioning

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import cn from "classnames"
 import "./Nav.css"
-import { Link } from 'react-router-dom';
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
       <nav className="navbar navbar-expand-lg navbar-dark">
         <Link className="navbar-brand" to="/">
           <img src="/assets/images/logoW.png" alt="brand"/>
         </Link>
-        <div className="collapse navbar-collapse" id="navToggler"> 
+        <div className={
+            cn(["collapse", "navbar-collapse"],{
+              show: isOpen
+            })
+            }
+            id="navToggler"> 
           <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
             <li className="nav-item">
               <Link to="/wdportfolio" className="nav-link btn" id="wdProjectsBtn">Web Development Projects</Link>
@@ -20,7 +29,16 @@ export const Nav = () => {
             </li>
           </ul>
         </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navToggler" aria-controls="navToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navToggler" 
+          aria-controls="navToggler" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
       </nav>
