dc7de32e490e926da510baf12cd9916e8352f481
wdfhai <wd.fhai@outlook.com>
2021-05-23 21:53:37 -0500

    added rel=noreferrer

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -19,11 +19,11 @@ export const Card = ({
         <p id="cardDesc" className="card-text">{description}</p>
       </div>
       <div className="card-footer mx-auto">
-        <a className="btn btn-primary" id="activeBtn" type="button" href={gitHub} target="_blank" rel="noreferror">Repo</a>
+        <a className="btn btn-primary" id="activeBtn" type="button" href={gitHub} target="_blank" rel="noreferrer">Repo</a>
         {
           {
-            "#": <a className="btn btn-danger disabled" id="inactiveBtn" type="button" href={deployed} rel="noreferror" target="_blank"><s>Deployed</s></a>            
-          }[deployed] || <a className="btn btn-primary" id="activeBtn" type="button" href={deployed} rel="noreferror" target="_blank">Deployed</a>
+            "#": <a className="btn btn-danger disabled" id="inactiveBtn" type="button" href={deployed} rel="noreferrer" target="_blank"><s>Deployed</s></a>            
+          }[deployed] || <a className="btn btn-primary" id="activeBtn" type="button" href={deployed} rel="noreferrer" target="_blank">Deployed</a>
         }
       </div>
     </div>
